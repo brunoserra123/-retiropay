911aa2928ab4e4cc098fe7e3227a0f930f48b499
RetiroPay Dev <retiropay@example.com>
2026-08-27 20:49:12 -0300

    refactor: flatten repository for native vercel support

--- backend/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { type Request } from 'express';
-export declare const JWT_SECRET = "retiro_super_secret_exportada";
-export interface AuthRequest extends Request {
-    user?: any;
-}
-//# sourceMappingURL=index.d.ts.map
