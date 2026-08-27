d886a34832cff3116eac189868aa4fff045b9a94
RetiroPay Dev <retiropay@example.com>
2026-08-27 20:53:12 -0300

    fix: remove conflicting compiled files in api folder

--- api/db.d.ts
+++
@@ -1,6 +0,0 @@
-import sqlite3 from 'sqlite3';
-export declare const dbRun: (sql: string, params?: any[]) => Promise<sqlite3.RunResult>;
-export declare const dbGet: <T>(sql: string, params?: any[]) => Promise<T | undefined>;
-export declare const dbAll: <T>(sql: string, params?: any[]) => Promise<T[]>;
-export declare const initDb: () => Promise<void>;
-//# sourceMappingURL=db.d.ts.map
